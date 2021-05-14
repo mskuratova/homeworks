7544c9cb004108f0fa0462d0854e6aace3e29b07
Moon <m.p.skuratova@gmail.com>
2021-05-14 18:01:41 +0600

    5 дз без вертски

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+            <NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR}></NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}></NavLink>
 
         </div>
     )
